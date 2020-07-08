ee92e1e8bcc44f8f5c1a51c433da503d73cb6a7f
Mitchelle Bengura <bengura.mitchell@gmail.com>
2020-07-08 19:37:49 +0800

    added MONGO_URI functionality

--- config/db.js
+++ config/db.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose');
 // const MongoClient = require('mongodb').MongoClient;
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://localhost/financedashboard', {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        });
+        const connection = await mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personalfinancedashboard",
+            { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+          );
 
         console.log(`MongoDB Connected: ${connection.connection.host}`.cyan.underline.bold);
     } catch(e) {
